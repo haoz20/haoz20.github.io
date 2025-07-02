f3a7b8323869d8dc490673256139298e27557084
haoz20 <zhaonianhao2005@gmail.com>
2025-07-02 10:37:46 +0700

    complete createing my-app

---
+++ my-app/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+function App() {
+  const [count, setCount] = useState(0)
+  return (
+    <div>
+        {count}
+        <button onClick={() => setCount((count) => count + 1)}>Increment</button>
+      </div>
+  )
+}
+export default App
